3a6dbe67a5dde887315dfdff44cd984f6f3a8ce9
edoardo <105643778+Scapizza@users.noreply.github.com>
2024-11-29 12:30:14 +0000

    modified:   app.py                  modificata per implementare nei return un titolo ed un nome randomico 	new file:   templates/about.html        creata pagina di informazioni che estende base 	new file:   templates/base.html         creata pagina base da estendere 	new file:   templates/contact.html      creata pagina di contatti che estende base 	new file:   templates/index.html        creata pagina home che estende base

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask,render_template
+import random
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    nomi = ["Roberto","Alberto","Samuele","Francesco"]
+    return render_template("index.html",titolo = "Homepage",nome = random.choice(nomi))
 
-app.run()
+@app.route("/about")
+def about():
+    nomi = ["Roberto","Alberto","Samuele","Francesco"]
+    return render_template("about.html",titolo = "About",nome = random.choice(nomi))
+
+@app.route("/contact")
+def contact():
+    nomi = ["Roberto","Alberto","Samuele","Francesco"]
+    return render_template("contact.html",titolo = "Contact",nome = random.choice(nomi))
+
+app.run(debug=True)
